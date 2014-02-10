d721ae5074bc0e850bc69808fde9ec7e91552ba1
Niclas Nilsson <niclas@niclasnilsson.se>
2014-02-10 12:31:33 +0100

    Removed export DYLD_FALLBACK_LIBRARY_PATH that killed JRuby

--- files/java.sh
+++ files/java.sh
@@ -3,7 +3,11 @@
 # Invoke java with the $DYLD_LIBRARY_PATH set with the homebrew lib dir.
 # This allows java to load native libraries installed via homebrew.
 
-export DYLD_FALLBACK_LIBRARY_PATH="$BOXEN_HOME/homebrew/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+# This export makes JRuby fail with 
+#   'NotImplementedError: getppid unsupported or native support failed to load'
+# Need to remove it for JRuby to work properly in our case.
+# 
+# export DYLD_FALLBACK_LIBRARY_PATH="$BOXEN_HOME/homebrew/lib:$DYLD_FALLBACK_LIBRARY_PATH"
 
 if [ -x /usr/libexec/java_home ]; then
   export JAVA_HOME=`/usr/libexec/java_home`
